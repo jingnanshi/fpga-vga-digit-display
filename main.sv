e0aecd82b808e8382096e88d0e2d45349dbb25bb
David E Olumese <dolumese@g.hmc.edu>
2018-11-30 00:22:48 -0800

    Updated logic to increment pix & line correctly
     - increment x (pixel) and y (line) seperate but in the same register block
     - updated parameters vertical back porch to 32 from 31
     - Status: getting on screen error => "H.V Frequency over Range"

--- main.sv
+++ main.sv
@@ -1,5 +1,5 @@
 // Top-level module of VGA digit display
-// Jingnan Shi
+// Jingnan Shi | David E Olumese
 // 11/26/2018
 // 
 // Some good references:
@@ -10,14 +10,16 @@
 // http://blog.andyselle.com/2014/12/04/vga-character-generator-on-an-fpga/
 // https://www.fpga4fun.com/PongGame.html
 module vga_digit_display(input logic clk,
-								 input logic reset,
-								 output logic hsync,
-								 output logic vsync,
-								 output logic VGA_R,
-								 output logic VGA_G,
-								 output logic VGA_B);
-	
-	// generates a 25 MHz pixel clock 
+			 input logic reset,
+			 output logic hSync,
+			 output logic vSync,
+			 output logic R,
+			 output logic G,
+			 output logic B);
+
+	// TODO: Consider using phased lock loop clk generate for more pression
+
+	// generates a 25 MHz pixel clock
 	// p = 5; N = 3;
 	// 40 MHz * p / 2^N = 25 MHz 
 	// (close to 25.175 MHz)
@@ -31,26 +33,27 @@ module vga_digit_display(input logic clk,
 	
 	// driver module
 	logic [9:0] x;
-	logic [8:0] y;
+	logic [9:0] y;
 	logic valid;
-	vga_driver driver(pix_clk, reset, hsync, vsync, x, y, valid);
+	vga_driver driver(pix_clk, reset, hSync, vSync, x, y, valid);
 
 	// draw a red square
-	assign VGA_R = ((x > 10'd200) && (y > 10'd120) && (x < 10'd360) && (y < 10'd280));
-	assign VGA_G = 0;
-	assign VGA_B = 0;
+	assign R = ((x > 10'd200) && (y > 10'd120) && (x < 10'd360) && (y < 10'd280));
+	assign G = 0;
+	assign B = 0;
 endmodule 
 
 
 // Module for generating x,y,hsync,vsync,valid
 // pix_clk: 25.175 MHz
-module vga_driver(input logic pix_clk, 
-						input logic reset, 
-						output logic hsync, 
-						output logic vsync,
-						output logic [9:0] x,
-						output logic [8:0] y,
-						output logic valid);
+// Screen Refresh Rate: 60 Hz
+module vga_driver(input logic pix_clk,
+		  input logic reset,
+		  output logic hSync,
+		  output logic vSync,
+		  output logic [9:0] x,
+		  output logic [9:0] y,
+		  output logic valid);
 						
 	// VGA Timing data: http://martin.hinner.info/vga/timing.html
 	// 
@@ -65,8 +68,8 @@ module vga_driver(input logic pix_clk,
 	// Active Video: 480
 	// Front Porch: 11
 	// Sync Pulse: 2
-	// Back Porch: 31
-	// Total lines: 524
+	// Back Porch: 32
+	// Total lines: 525
 	// Horizontal timing information
 	localparam H_ACTIVE_VIDEO = 10'd640;
 	localparam H_FRONT_PORCH = 10'd16;
@@ -75,43 +78,37 @@ module vga_driver(input logic pix_clk,
 	localparam H_END = H_ACTIVE_VIDEO + H_FRONT_PORCH + H_SYNC_PULSE + H_BACK_PORCH;
 	
 	// Vertical timing information
-	localparam V_ACTIVE_VIDEO = 9'd480;
-	localparam V_FRONT_PORCH = 9'd11;
-	localparam V_SYNC_PULSE = 9'd2;
-	localparam V_BACK_PORCH = 9'd31;
+	localparam V_ACTIVE_VIDEO = 10'd480;
+	localparam V_FRONT_PORCH = 10'd11;
+	localparam V_SYNC_PULSE = 10'd2;
+	localparam V_BACK_PORCH = 10'd32;
 	localparam V_END = V_ACTIVE_VIDEO + V_FRONT_PORCH + V_SYNC_PULSE + V_BACK_PORCH;
 	
 	// hsync is low at (h active video + h front porch, 
-   //                  h active video + h front porch + h sync pulse)	
+	// h active video + h front porch + h sync pulse)	
 	assign hsync = ~((x >= (H_ACTIVE_VIDEO + H_FRONT_PORCH))
-						& (x <  (H_ACTIVE_VIDEO + H_FRONT_PORCH + H_SYNC_PULSE)));
+			& (x <  (H_ACTIVE_VIDEO + H_FRONT_PORCH + H_SYNC_PULSE)));
 
 	// vsync is low at (v active video + v front porch, 
-	//                  v active video + v front porch + v sync pulse)
-	assign vsync = ~((y >= (V_ACTIVE_VIDEO + V_FRONT_PORCH)) 
-						& (y <  (V_ACTIVE_VIDEO + V_FRONT_PORCH + V_SYNC_PULSE)));
+	// v active video + v front porch + v sync pulse)
+	assign vsync = ~((y >= (V_ACTIVE_VIDEO + V_FRONT_PORCH))
+			& (y <  (V_ACTIVE_VIDEO + V_FRONT_PORCH + V_SYNC_PULSE)));
 	
 	// Valid region of pixels: x < H_ACTIVE_VIDEO & y < V_ACTIVE_VIDEO
 	assign valid = (x < H_ACTIVE_VIDEO) & (y < V_ACTIVE_VIDEO);
 	
-	// Generate x
+	// Generate x and y
 	always @(posedge pix_clk, posedge reset) begin
 		if (reset) begin
-			x <= 0;
+			x <= 0; y <= 0;
 		end else begin
-			if (x == H_END) x<= 0;
-			else x<=x + 10'd1;
+			x <= x + 10'd1;         // increment pixel
+			if (x >= H_END) begin 
+				x <= 0;
+				y <= y + 10'd1; // increment line 
+			end
+
+			if (y >= V_END) y <= 0;
 		end
 	end
-	
-	// Generate y
-	always @(posedge pix_clk, posedge reset) begin
-		if (reset) begin
-			y <= 0;
-		end else begin
-			if (y == V_END) y<= 0;
-			else y<=y + 9'd1;
-		end
-	end
-	
 endmodule 
