e8455fcc5fe7a7c2eaacd0e5181e99b76ddea134
David E Olumese <dolumese@g.hmc.edu>
2018-11-30 01:02:49 -0800

    Update spacing

--- main.sv
+++ main.sv
@@ -17,32 +17,30 @@ module vga_digit_display(input  logic clk,
 			 output logic G,
 			 output logic B);
 
-	// TODO: Consider using phased lock loop clk generate for more pression
+  // TODO: Consider using phased lock loop clk generate for more pression
 
-	// generates a 25 MHz pixel clock
-	// p = 5; N = 3;
-	// 40 MHz * p / 2^N = 25 MHz 
-	// (close to 25.175 MHz)
-	logic [2:0] q;
-	always_ff @(posedge clk, posedge reset) begin
-		if (reset) q <= 0;
-		else       q <= q + 3'd5;
-	end
-	logic pix_clk;
-	assign pix_clk = q[2];
+  // generates a 25 MHz pixel clock
+  // p = 5; N = 3;
+  // 40 MHz * p / 2^N = 25 MHz 
+  // (close to 25.175 MHz)
+  logic [2:0] q;
+  always_ff @(posedge clk, posedge reset) begin
+    if (reset) q <= 0;
+    else       q <= q + 3'd5;
+  end
+
+  logic pix_clk; assign pix_clk = q[2];
 	
-	// driver module
-	logic [9:0] x;
-	logic [9:0] y;
-	logic valid;
-	vga_driver driver(pix_clk, reset, hSync, vSync, x, y, valid);
+  // driver module
+  logic [9:0] x, y;
+  logic valid;
+  vga_driver driver(pix_clk, reset, hSync, vSync, x, y, valid);
 
-	// draw a red square
-	assign R = ((x > 10'd200) && (y > 10'd120) && (x < 10'd360) && (y < 10'd280));
-	assign G = 0;
-	assign B = 0;
+  // draw a red square
+  assign R = ((x > 10'd200) && (y > 10'd120) && (x < 10'd360) && (y < 10'd280));
+  assign G = 0;
+  assign B = 0;
 endmodule 
-
 
 // Module for generating x,y,hsync,vsync,valid
 // pix_clk: 25.175 MHz
@@ -74,28 +72,28 @@ module vga_driver#(parameter H_AV  = 10'd640,
 		  output logic [9:0] y,
 		  output logic       valid);
 						
-	// hSync is low for [H_AV + H_FP, H_AV + H_FP + H_SP]	
-	assign hsync = ~((x >= (H_AV + H_FP)) & (x < (H_AV + H_FP + H_SP)));
+  // hSync is low for [H_AV + H_FP, H_AV + H_FP + H_SP]	
+  assign hsync = ~((x >= (H_AV + H_FP)) & (x < (H_AV + H_FP + H_SP)));
 
-	// vSync is low for [V_AV + V_FP, V_AV + V_FP + V_SP]	
-	assign vsync = ~((y >= (V_AV + V_FP)) & (y < (V_AV + V_FP + V_SP)));
-	
-	// Valid region of pixels: x < H_ACTIVE_VIDEO & y < V_ACTIVE_VIDEO
-	assign valid = (x < H_AV) & (y < V_AV);
-	
-	// Generate x and y
-	always @(posedge pix_clk, posedge reset) begin
-		if (reset) begin
-			x <= 0;
-			y <= 0;
-		end else begin
-			x <= x + 10'd1;         // increment pixel
-			if (x >= H_END) begin 
-				x <= 0;
-				y <= y + 10'd1; // increment line 
+  // vSync is low for [V_AV + V_FP, V_AV + V_FP + V_SP]	
+  assign vsync = ~((y >= (V_AV + V_FP)) & (y < (V_AV + V_FP + V_SP)));
 
-				if (y >= V_END) y <= 0;
-			end
-		end
-	end
+  // Valid region of pixels: x < H_ACTIVE_VIDEO & y < V_ACTIVE_VIDEO
+  assign valid = (x < H_AV) & (y < V_AV);
+
+  // Generate x and y
+  always @(posedge pix_clk, posedge reset) begin
+    if (reset) begin
+      x <= 0;
+      y <= 0;
+    end else begin
+      x <= x + 10'd1;           // increment pixel count
+      if (x >= H_END) begin
+        x <= 0;                 // reset pixel count
+        y <= y + 10'd1;         // increment line count 
+
+        if (y >= V_END) y <= 0; // reset line count
+      end
+    end
+  end
 endmodule
