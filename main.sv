9d67e96c7992c395b57a2fdc0635a5cafc41f246
David E Olumese <dolumese@g.hmc.edu>
2018-11-30 00:39:43 -0800

    Clean up: module params instead of local params

--- main.sv
+++ main.sv
@@ -9,8 +9,8 @@
 // https://dekunukem.wordpress.com/2016/03/04/putting-the-f-in-fap-vga-controller-part-1-character-generator/
 // http://blog.andyselle.com/2014/12/04/vga-character-generator-on-an-fpga/
 // https://www.fpga4fun.com/PongGame.html
-module vga_digit_display(input logic clk,
-			 input logic reset,
+module vga_digit_display(input  logic clk,
+			 input  logic reset,
 			 output logic hSync,
 			 output logic vSync,
 			 output logic R,
@@ -47,55 +47,41 @@ endmodule
 // Module for generating x,y,hsync,vsync,valid
 // pix_clk: 25.175 MHz
 // Screen Refresh Rate: 60 Hz
-module vga_driver(input logic pix_clk,
-		  input logic reset,
-		  output logic hSync,
-		  output logic vSync,
+//
+// VGA Timing data: http://martin.hinner.info/vga/timing.html
+// Horizontal => Pixels
+// Vertical => Lines
+// AV  => Active Video
+// FP  => Front Porch
+// SP  => Sync Pulse
+// BP  => Back Porch
+// END => Total pixels
+module vga_driver#(parameter H_AV  = 10'd640,
+			     H_FP  = 10'd16,
+			     H_SP  = 10'd96, 
+			     H_BP  = 10'd48,
+			     H_END = H_AV + H_FP + H_SP + H_BP,
+			     V_AV  = 10'd480,
+			     V_FP  = 10'd11,
+			     V_SP  = 10'd2, 
+			     V_BP  = 10'd32,
+			     V_END = V_AV + V_FP + V_SP + V_BP)
+		 (input  logic       pix_clk,
+		  input  logic       reset,
+		  output logic       hSync,
+		  output logic       vSync,
 		  output logic [9:0] x,
 		  output logic [9:0] y,
-		  output logic valid);
+		  output logic       valid);
 						
-	// VGA Timing data: http://martin.hinner.info/vga/timing.html
-	// 
-	// Horizontal Pixel Timings
-	// Active Video: 640
-	// Front Porch: 16
-	// Sync Pulse: 96
-	// Back Porch: 48
-	// Total pixels: 800
-	//
-	// Vertical Line Timings
-	// Active Video: 480
-	// Front Porch: 11
-	// Sync Pulse: 2
-	// Back Porch: 32
-	// Total lines: 525
-	// Horizontal timing information
-	localparam H_ACTIVE_VIDEO = 10'd640;
-	localparam H_FRONT_PORCH = 10'd16;
-	localparam H_SYNC_PULSE = 10'd96;
-	localparam H_BACK_PORCH = 10'd48;
-	localparam H_END = H_ACTIVE_VIDEO + H_FRONT_PORCH + H_SYNC_PULSE + H_BACK_PORCH;
-	
-	// Vertical timing information
-	localparam V_ACTIVE_VIDEO = 10'd480;
-	localparam V_FRONT_PORCH = 10'd11;
-	localparam V_SYNC_PULSE = 10'd2;
-	localparam V_BACK_PORCH = 10'd32;
-	localparam V_END = V_ACTIVE_VIDEO + V_FRONT_PORCH + V_SYNC_PULSE + V_BACK_PORCH;
-	
-	// hsync is low at (h active video + h front porch, 
-	// h active video + h front porch + h sync pulse)	
-	assign hsync = ~((x >= (H_ACTIVE_VIDEO + H_FRONT_PORCH))
-			& (x <  (H_ACTIVE_VIDEO + H_FRONT_PORCH + H_SYNC_PULSE)));
+	// hSync is low for [H_AV + H_FP, H_AV + H_FP + H_SP]	
+	assign hsync = ~((x >= (H_AV + H_FP)) & (x < (H_AV + H_FP + H_SP)));
 
-	// vsync is low at (v active video + v front porch, 
-	// v active video + v front porch + v sync pulse)
-	assign vsync = ~((y >= (V_ACTIVE_VIDEO + V_FRONT_PORCH))
-			& (y <  (V_ACTIVE_VIDEO + V_FRONT_PORCH + V_SYNC_PULSE)));
+	// vSync is low for [V_AV + V_FP, V_AV + V_FP + V_SP]	
+	assign vsync = ~((y >= (V_AV + V_FP)) & (y < (V_AV + V_FP + V_SP)));
 	
 	// Valid region of pixels: x < H_ACTIVE_VIDEO & y < V_ACTIVE_VIDEO
-	assign valid = (x < H_ACTIVE_VIDEO) & (y < V_ACTIVE_VIDEO);
+	assign valid = (x < H_AV) & (y < V_AV);
 	
 	// Generate x and y
 	always @(posedge pix_clk, posedge reset) begin
