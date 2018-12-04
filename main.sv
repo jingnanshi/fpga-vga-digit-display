002a5b01966d238eb29471445e2cae3f8bc211a3
David E Olumese <dolumese@g.hmc.edu>
2018-12-03 23:26:22 -0800

    Hardware tested and functional

--- main.sv
+++ main.sv
@@ -1,21 +1,16 @@
+//
 // Top-level module of VGA digit display
 // Jingnan Shi | David E Olumese
 // 11/26/2018
 // 
-// Some good references:
-// https://timetoexplore.net/blog/arty-fpga-vga-verilog-01
-// https://www.digikey.com/eewiki/pages/viewpage.action?pageId=15925278
-// http://www.righto.com/2018/04/fizzbuzz-hard-way-generating-vga-video.html
-// https://dekunukem.wordpress.com/2016/03/04/putting-the-f-in-fap-vga-controller-part-1-character-generator/
-// http://blog.andyselle.com/2014/12/04/vga-character-generator-on-an-fpga/
-// https://www.fpga4fun.com/PongGame.html
-module vga_digit_display(input  logic clk, reset,
-			 output logic hSync, vSync, R, G, B);
+
+module vga_digit_display(input  logic       clk, reset,
+			 input  logic [3:0] digit,
+			 input  logic       digitEn,
+			 output logic       hSync, vSync, R, G, B);
 
   logic [9:0] x, y;
   logic       valid;
-  logic [3:0] digit; assign digit = 4'd0;  // TODO: Make into an input
-  logic       digitEn; assign digitEn = 1; // TODO: Make into an input
   
   // Create 25.175 MHz pixel clock for the VGA
   vga_pll vga_pll(.inclk0(clk), .c0(pixClk));
@@ -24,7 +19,7 @@ module vga_digit_display(input  logic clk, reset,
   vga_driver driver(pixClk, reset, hSync, vSync, x, y, valid);
 
   // generate video
-  video_gen video(digit, digitEn, x, y, R, G, B);
+  video_gen video(clk, reset, digit, digitEn, x, y, R, G, B);
 endmodule 
 
 // Module for generating x, y, hsync, vsync, valid
@@ -78,14 +73,15 @@ module vga_driver#(parameter H_AV  = 10'd640,
   end
 endmodule
 
-// Generates the video signals (digit in white & text in blue)
-module video_gen(input  logic [3:0] digit,
+// Generates the video signals (digit in white & text in cyan)
+module video_gen(input  logic       clk, reset,
+		 input  logic [3:0] digit,
 		 input  logic       digitEn,
 		 input  logic [9:0] x, y,
 		 output logic       R, G, B);
 
   logic       digPix, txtPix;
-  logic [3:0] txtSelect; // chooses which of the 10 strings to display
+  logic [3:0] txtSelect;      // chooses which of the 10 strings to display
 
   text_select_lfsr_rng tslr(clk, reset, digit, digitEn, txtSelect);
 
@@ -93,38 +89,45 @@ module video_gen(input  logic [3:0] digit,
   txt_gen_rom tgr(txtSelect, x, y, txtPix);
 
   // Produce RGB signals
-  assign {R, G, B} = {digPix, digPix, (digPix | txtPix)};
+  assign {R, G, B} = {digPix, (digPix | txtPix), (digPix | txtPix)};
 
 endmodule
 
 // A 5-bit LFSR puesdo-number generator
 //  Holds selection until digit changes
+//   https://www.eetimes.com/document.asp?doc_id=1274550&page_number=2 => for maximal LFSR polynomial
 module text_select_lfsr_rng#(parameter OPTIONS = 4'd10) // Number strings to be selected from
 			   (input  logic       clk, reset,
 			    input  logic [3:0] digit,
 			    input  logic       digitEn,
 			    output logic [3:0] txtSelect);
 
-  logic [3:0] digitPrev, tempSelect;
-  logic       en;
+  logic [4:0] q;
+  logic [3:0] digitPrev;
+  logic [2:0] p;
+  logic       en, sclk;
 
-  // Remember the previous digit
-  always_ff @(negedge clk)
+  // generate a slower clk (10MHz)
+  always_ff @(posedge clk, posedge reset)
+    if (reset) p <= 3'd0;
+    else       p <= p + 3'd1;
+  assign sclk = p[2];
+
+  // remember the previous digit
+  always_ff @(negedge sclk)
     digitPrev <= digit;
 
-  // Select new text if digit changes
-  assign en = digitPrev != digit;
+  assign en = digitPrev != digit; // select new text if digit changes
 
   always_ff @(posedge clk, posedge reset) begin
     if (reset)   q <= 4'd3;                  // initial seed (non-zero)
-    else if (en) q <= {q[3:0], q[3] ^ q[0]}; // polynomial for maximal LFSR
+    else if (en) q <= {q[3:0], q[4] ^ q[1]}; // polynomial for maximal LFSR
     else         q <= q;
   end
 
-  assign tempSelect = {q[4], q[2:0]}; // omit 4th bit ("more" randomness)
-  assign txtSelect  = (digitEn) ? ((tempSelect > 4'd0 & tempSelect < OPTIONS) ?
-                                    tempSelect : 4'd1)
-				 : 4'd0; // 0 for instructions
+  assign txtSelect = (digitEn) ? ((q[3:0] > 4'd0 & q[3:0] < OPTIONS) ?
+                                   q[3:0] : {1'b0, q[3:1])
+				 : 4'd0;     // 0 for instructions
 
 endmodule
 
@@ -135,16 +138,16 @@ module dig_gen_rom#(parameter SIZE    = 10'd320,
 			      X_END   = X_START + SIZE - 10'd2, // offset due to division resolution
 			      X_DIV   = 10'd53,  // SIZE / 6 (cols of digit)
 			      Y_START = 10'd20,
-			      Y_END   = X_START + SIZE,
+			      Y_END   = Y_START + SIZE,
 			      Y_DIV   = 10'd40)  // SIZE / 8 (rows of digit)
                   (input  logic [3:0] digit,
 		   input  logic       digitEn,
 		   input  logic [9:0] x, y,
                    output logic       pixel);
 
-  logic [5:0] digrom[127:0]; // digit generator ROM (8 lines/digit * 10 digit)
-  logic [5:0] line;          // a line of the digit
-  logic [2:0] xoff, yoff;    // current position in the digit
+  logic [5:0] digrom[79:0]; // digit generator ROM (8 lines/digit * 10 digit)
+  logic [5:0] line;         // a line of the digit
+  logic [2:0] xoff, yoff;   // current position in the digit
   logic       valid;
 
   // initialize the digit ROM from file
@@ -172,17 +175,17 @@ endmodule
 module txt_gen_rom#(parameter SCALE    = 10'd2,
                               WIDTH    = 10'd12,
                               HEIGHT   = 10'd16,
-			      X_END    = 10'd640,
+			      X_END    = 10'd636, // WIDTH * TXT_SIZE
 			      Y_START  = 10'd412, // DIGIT_Y_END + 52
 			      Y_END    = Y_START + HEIGHT,
 			      TXT_SIZE = 6'd53)
 		  (input  logic [3:0] txtSelect, 
-		   input  logic       x, y,
+		   input  logic [9:0] x, y,
                    output logic       pixel);
 
-  logic [5:0] charrom[255:0]; // character generator ROM (8 lines/char * 29 char)
-  logic [4:0] txtrom[1023:0]; // formatted text ROM (53 char/text * 10 text)
-  logic [4:0] char;           // character to display
+  logic [5:0] charrom[231:0]; // character generator ROM (8 lines/char * 29 char)
+  logic [7:0] txtrom[541:0];  // formatted text ROM (number of lines in file)
+  logic [7:0] char;           // character to display
   logic [5:0] charPos;        // position of character screen
   logic [9:0] txtPos;         // position of text in ROM
   logic [2:0] xoff, yoff;
@@ -205,7 +208,7 @@ module txt_gen_rom#(parameter SCALE    = 10'd2,
 
   // extract the current line from the desired character
   //  6x8 character; char * 8 + curr_y gives the line from ROM
-	assign line = {charrom[yoff+{char, 3'b000}]};
+  assign line = {charrom[yoff+{char, 3'b000}]};
 
   // reverse the bit order and extract current pixel
   assign pixel = (valid) ? line[3'd5 - xoff] :  1'd0;
